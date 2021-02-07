71f1ba23aead9bfc7b4ce01dbab65a0396805f8b
BrunoConti <bruno.conti1997@gmail.com>
2021-02-07 18:16:08 -0300

    Agrego Breadcrumb

--- src/components/Breadcrumb.js
+++ src/components/Breadcrumb.js
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Breadcrumb = (props) => {
+const Breadcrumb = () => {
+  const [categories] = useState(
+    JSON.parse(localStorage.getItem("categories")) || []
+  );
+  console.log("categories", categories);
   return (
     <div className="breadcrumb--container">
-      <p>Breadcrumb</p>
+      <p>{categories?.join(" > ")}</p>
     </div>
   );
 };
